2e2c91d9a395e2c91b547114d8b0a6f5c16931a5
EloyPardo <pardogurreaeloy@gmail.com>
2025-01-10 11:35:13 +0100

    Práctica Login EN PROCESO

---
+++ Tema3/Login/src/main/java/org/example/login/controllers/MainController.java
@@ -0,0 +1,26 @@
+package org.example.login.controllers;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+
+    @GetMapping("/")
+    public String index(){
+        return "index";
+    }
+
+    @GetMapping("/private")
+    public String privateZone(Model model){
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String username = authentication.getName();
+
+        model.addAttribute("username", username);
+        return "private";
+    }
+
+}
