f3b841cb96340fbe95c2bda4399ffca7b74bad14
Eloypg <pardogurreaeloy@gmail.com>
2025-02-22 13:34:43 +0100

    Practica Codigos HTTP -> HECHA

--- Tema3/CodigosHTTP/src/main/java/org/example/codigoshttp/controllers/CodigosHTTPController.java
+++ Tema3/CodigosHTTP/src/main/java/org/example/codigoshttp/controllers/CodigosHTTPController.java
@@ -1,35 +1,59 @@
 package org.example.codigoshttp.controllers;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
-
-import static org.springframework.http.HttpStatus.*;
 
 @Controller
 public class CodigosHTTPController {
 
     @GetMapping("/")
-    public String index() {
-        return "index";
-    }
-    @GetMapping("/error/404")
-    public String err404() {
-        throw new ResponseStatusException(NOT_FOUND, "Unable to find resource");
-    }
-    @GetMapping("/error/401")
-    public String err401() {
-        throw new ResponseStatusException(UNAUTHORIZED, "Not authorized");
-    }
-    @GetMapping("/error/403")
-    public String err403() {
-        throw new ResponseStatusException(FORBIDDEN, "Permissions denied");
-    }
-    @GetMapping("500")
-    public String err500() {
-        throw new ResponseStatusException(INTERNAL_SERVER_ERROR, "Server error");
+    public String result(){
+        return "result";
     }
 
+    @GetMapping("/suma/{num1}/{num2}")
+    public String suma(@PathVariable int num1, @PathVariable int num2, Model model) {
+        model.addAttribute("operation", "Suma");
+        model.addAttribute("result", num1 + num2);
+        return "result";
+    }
 
+    @GetMapping("/division/{num1}/{num2}")
+    public String division(@PathVariable int num1, @PathVariable int num2, Model model) {
+        if (num2 == 0) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "No se puede dividir por cero");
+        }
+        model.addAttribute("operation", "División");
+        model.addAttribute("result", (double) num1 / num2);
+        return "result";
+    }
 
+    @GetMapping("/raiz/{num}")
+    public String raiz(@PathVariable int num, Model model) {
+        throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "Acceso no autorizado a la ruta /raiz");
+    }
+
+    @GetMapping("/factorial/{num}")
+    public String factorial(@PathVariable int num, Model model) {
+        if (num < 0) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "No existe factorial para números negativos");
+        }
+        if (num > 100) {
+            throw new ResponseStatusException(HttpStatus.FORBIDDEN, "No se permite calcular factorial de números mayores a 100");
+        }
+        model.addAttribute("operation", "Factorial");
+        model.addAttribute("result", calcularFactorial(num));
+        return "result";
+    }
+
+    private long calcularFactorial(int num) {
+        long result = 1;
+        for (int i = 2; i <= num; i++) {
+            result *= i;
+        }
+        return result;
+    }
 }
